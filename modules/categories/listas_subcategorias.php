e24e73d0602f92f2f2c410c3c44764bf78130a95
evelin0521 <f.gongoraandrade@gmail.com>
2021-12-14 17:45:44 -0500

    formularios editados visualmente

--- modules/categories/listas_subcategorias.php
+++ modules/categories/listas_subcategorias.php
@@ -14,7 +14,7 @@
 			echo "<div class=container>";
 			echo "<h1 class=text-center>Listado de subcategorias</h1>";
 			echo "<form action='index.php' method='GET'>
-			<button class=btn>
+			<button class=btn title='Atrás'>
 				<img src='img/1486348529-back-backwards-repeat-arrows-arrow_80455 (1).ico' width=30px/>
 			</button>";
 			echo "<br>";
