98e850328ef7073a03597f7ced1a1c2614046d53
evelin0521 <f.gongoraandrade@gmail.com>
2021-12-14 16:16:39 -0500

    sin fondos el modulo de categorias

--- modules/categories/formNewSubcategoria.php
+++ modules/categories/formNewSubcategoria.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Nueva categoria</title>
+	<link rel="stylesheet" href="../../css/contentBackground.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css">
 	<script type="text/javascript">
@@ -11,7 +12,7 @@
 		}
 	</script>
 </head>
-<body>             
+<body class="bg-secondary-custom">             
 	<div class="container">	
 		<h1 class="text-center">Nueva SUBcategoria</h1>
 		<form action="cod/newsubcategorias.php" method="GET">
--- modules/categories/listadocategorias.php
+++ modules/categories/listadocategorias.php
@@ -1,93 +1,92 @@
-<?php
-    /////aqui va algo/////
-?>
-
+<!DOCTYPE html>
+<html>
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="../../css/contentBackground.css">
 </head>
-
-
-<?php
-	require'cod/categoria_sub.php';
-    $cat = new Categorias();
-    $result = $cat->consulta();
-    $result = $cat->consultacategorias();
-	$cont=0;
-    
-    echo "<div class=container>";
-		echo "<h1 class=text-center>categorias</h1>";
-		echo "<form action='index.php' method='GET'>
-		<button class=btn>
-			<img src='img/1486348529-back-backwards-repeat-arrows-arrow_80455 (1).ico' width=30px/>
-			<a>Atras</a>
-		</button>";
+<body class="bg-secondary-custom">
+	<?php
+		require'cod/categoria_sub.php';
+		$cat = new Categorias();
+		$result = $cat->consulta();
+		$result = $cat->consultacategorias();
+		$cont=0;
 		
-		echo "<br>";
-		echo "<a href=formnewCategoria.php class=btn>Agregar nueva categorias</a>";
-		echo "<table border=1 class=table color='black'>";
-		echo "<tr>
-				<th>#</th>
-				<th>IdCategorias</th>
-				<th>nombre</th>
-				<th>Estado</th>
-				<th>usuario a modificar</th>
-				<th>fecha creacion</th>
-				<th>fecha modifica</th>
-				<th>IDtipo</th>
-				<th>elimar</th>
-				
-			</tr>";
+		echo "<div class=container>";
+			echo "<h1 class=text-center>categorias</h1>";
+			echo "<form action='index.php' method='GET'>
+			<button class=btn>
+				<img src='img/1486348529-back-backwards-repeat-arrows-arrow_80455 (1).ico' width=30px/>
+				<a>Atras</a>
+			</button>";
+			
+			echo "<br>";
+			echo "<a href=formnewCategoria.php class=btn>Agregar nueva categorias</a>";
+			echo "<table border=1 class=table color='black'>";
+			echo "<tr>
+					<th>#</th>
+					<th>IdCategorias</th>
+					<th>nombre</th>
+					<th>Estado</th>
+					<th>usuario a modificar</th>
+					<th>fecha creacion</th>
+					<th>fecha modifica</th>
+					<th>IDtipo</th>
+					<th>elimar</th>
+					
+				</tr>";
 
-		while ($datos = mysqli_fetch_assoc($result)){
-			if ($datos['usuarioModifica']== ""){
-				$datos['usuarioModifica']= 'no registra';
-			}
-			if ($datos['fechaModificacion']== ""){
-				$datos['fechaModificacion']= 'no regstra';
-			}
-			if($datos['estado']== 8){
-				$datos['estado']='Activo';
-			}
-			elseif($datos['estado']== 9){
-				$datos['estado']='Inactivo';
-			}
-			// print_r($datos);
-			$cont= 1+$cont;
-			echo "<tr>";
-			echo "<td>".$cont."#</td>";
-				foreach ($datos as $key => $value) {
-					
-					
-					echo "<td>".$value."</td>";
-					
-					
+			while ($datos = mysqli_fetch_assoc($result)){
+				if ($datos['usuarioModifica']== ""){
+					$datos['usuarioModifica']= 'no registra';
 				}
-				echo "<td>
-					<form action='cod/eliminarCategoria.php' method='GET'>
-						<input type=hidden name=id value= ".$datos['idTipos'].">
-						<button class=btn>
-							<img src='https://img.icons8.com/ios/50/000000/delete--v3.png' width=30px/>
-						</button>
-					</form>
-					</td>";
+				if ($datos['fechaModificacion']== ""){
+					$datos['fechaModificacion']= 'no regstra';
+				}
+				if($datos['estado']== 8){
+					$datos['estado']='Activo';
+				}
+				elseif($datos['estado']== 9){
+					$datos['estado']='Inactivo';
+				}
+				// print_r($datos);
+				$cont= 1+$cont;
+				echo "<tr>";
+				echo "<td>".$cont."#</td>";
+					foreach ($datos as $key => $value) {
+						
+						
+						echo "<td>".$value."</td>";
+						
+						
+					}
+					echo "<td>
+						<form action='cod/eliminarCategoria.php' method='GET'>
+							<input type=hidden name=id value= ".$datos['idTipos'].">
+							<button class=btn>
+								<img src='https://img.icons8.com/ios/50/000000/delete--v3.png' width=30px/>
+							</button>
+						</form>
+						</td>";
 
-				echo"<td>
-					<form action='filtrarCategorias.php' method='GET'>
-						<input type=hidden name=id value= ".$datos['idTipos'].">
-						<button class=btn>
-							<img src='img/gonext_103393.ico' width=30px/>
-						</button>
-					</form>
-					</td>";
-			echo "</tr>";
-			
-			
-		}
-		if(isset($_GET['msn'])){
-			echo $_GET['msn'];
-		}
-	
-		echo "</table>";
-	echo "</div>";
-
-?>
+					echo"<td>
+						<form action='filtrarCategorias.php' method='GET'>
+							<input type=hidden name=id value= ".$datos['idTipos'].">
+							<button class=btn>
+								<img src='img/gonext_103393.ico' width=30px/>
+							</button>
+						</form>
+						</td>";
+				echo "</tr>";
+				
+				
+			}
+			if(isset($_GET['msn'])){
+				echo $_GET['msn'];
+			}
+		
+			echo "</table>";
+		echo "</div>";
+	?>
+</body>
+</html>
