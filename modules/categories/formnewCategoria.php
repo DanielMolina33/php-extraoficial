0178cb9c40efd46a32870e1da727fb5f41945e9b
evelin0521 <f.gongoraandrade@gmail.com>
2021-12-14 17:26:26 -0500

    clear

--- modules/categories/formnewCategoria.php
+++ modules/categories/formnewCategoria.php
@@ -3,8 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Nueva categoria</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
 	<link rel="stylesheet" href="../../css/contentBackground.css">
 	<script type="text/javascript">
 		function volver() {
@@ -13,21 +12,64 @@
 	</script>
 </head>
 <body class="bg-secondary-custom">             
-	<div class="container">	
-		<h1 class="text-center">Nueva categoria</h1>
-		<form action="cod/newcategoria.php" method="GET">
-			<input  type="text" name="descripcion" placeholder="Descripcion Categoria"><br>
-			<select name="estado">
+	<section class="row h-100 w-100 d-flex flex-column align-items-center m-0 mt-5">
+      <div class="col-12 col-lg-6 mb-4">
+        <h3 class="display-4 text-center">Formulario categoria</h3>
+      </div>
+      <div class="col-12 col-lg-6">
+        <form
+        action="cod/newcategoria.php"
+        method="POST">
+          <div class="mb-3">
+            <label for="total" class="w-100">
+            <input
+              type="text"
+              required="required"
+              name="descripcion"
+              id="descripcion"
+              placeholder="Descripcion Categoria"
+              class="form-control"
+            />
+            </label>
+          </div>
+          
+          <div class="mb-3">
+            <label for="product" class="w-100">
+			  <select  required="required" id="estado" class="form-select" name="estado">
 				<option selected value="0"> Elige una opción </option>
 				<option value="8">Activo</option> 
 				<option value="9">Inactivo</option>
-			</select> 			
-			Indique la fecha de creacion<br><input class="m-2" type="date" name="fechaCreacion" placeholder="Fecha Creacion"><br>
+				</select> 
+            </label>
+          </div>
+          
 
-			<input  type="submit" name="" placeholder="guardar">
-			<input type="button" name="" value="Volver" onclick="volver()">
-		</form><br>
-	</div>
-	
+		  <div class="mb-3">
+            <label for="amount" class="w-100">
+            <input
+              type="text"
+              required="required"
+              name="fechaCreacion"
+			  placeholder="Fecha creación"
+              id="fechaCreacion"
+              class="form-control"
+			  onfocus="this.type='date'"
+			  onblur="this.type='text'"
+            />
+            </label>
+          </div>
+
+          <div class="mb-3">
+            <input type="submit" value="Enviar" class="btn bg-dark text-white w-100"/>
+          </div>
+          <div>
+            <input type="reset" value="Borrar" class="btn bg-dark text-white w-100"/>
+          </div>
+        </form>
+      </div>  
+    </section>
+
+
+
 </body>
 </html
