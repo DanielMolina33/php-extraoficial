b085b5dc7e02baf0536885cc89b9db3777963502
evelin0521 <f.gongoraandrade@gmail.com>
2021-12-16 22:06:15 -0500

    se creo el menu de categorias

--- modules/categories/index.php
+++ modules/categories/index.php
@@ -1,27 +1,44 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="../../css/contentBackground.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link href="../../css/contentBackground.css" rel="stylesheet"/>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+  <title>Menú</title>
+  <style>
+    a:hover {
+      text-decoration: underline !important;
+    }
+  </style>
 </head>
 <body class="bg-secondary-custom">
-	<?php
-		echo"
-		<form action='listadocategorias.php' method='GET'>
-			<button class=btn>
-				<img src='img/tags_categories_icon_145927.ico' width=120px/>
-				categorias
-			</button>
-		</form>
-		";
-		echo"
-		<form action='listas_subcategorias.php' method='GET'>
-			<button class=btn>
-				<img src='img/discount_tags_icon_152101.ico' width=120px/>
-				subcategorias
-			</button>
-		</form>
-		";
-	?>
+  <section class="container mt-3"> 
+    <div class="row">
+      <div class="col-12">
+        <h2 class="fs-3 fw-light mb-3">Categorias</h2>
+        <ul class="list-group mb-4 mx-3">
+          <li class="list-group-item bg-transparent border-0">
+            <a href="formnewCategoria.php" class="text-decoration-none d-block text-secondary">Crear</a>
+          </li>
+          <li class="list-group-item bg-transparent border-0">
+            <a href="listadocategorias.php" class="text-decoration-none d-block text-secondary">Listado</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-12">
+        <h2 class="fs-3 fw-light mb-3">Subcategorias</h2>
+        <ul class="list-group mx-3">
+          <li class="list-group-item bg-transparent border-0">
+            <a href="formNewSubcategoria.php" class="text-decoration-none d-block text-secondary">Crear</a>
+          </li>
+          <li class="list-group-item bg-transparent border-0">
+            <a href="listas_subcategorias.php" class="text-decoration-none d-block text-secondary">Listado</a>
+          </li>
+        </ul>
+    </div>
+  </section>
 </body>
 </html>
+
